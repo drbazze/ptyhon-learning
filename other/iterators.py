d80a2b8218f03463da38f9adbc4fb1f2a3ea2484
Tamas Zumpf <1145394+drbazze@users.noreply.github.com>
2023-04-21 15:33:33 +0400

    Add example of an iterator

---
+++ other/iterators.py
@@ -0,0 +1,11 @@
+#Iterable
+mystr = "banana"
+#The iterator of the iterable
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
